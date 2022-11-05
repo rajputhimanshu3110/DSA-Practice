a1b77ed898a06945bd4cae0bf71faf7887db2fd1
Thakur Viraj Singh <75830024+singhhimanshu31@users.noreply.github.com>
2022-11-05 23:30:27 +0530

    Create PainterPart.cpp

---
+++ Searching & Sorting/PainterPart.cpp
@@ -0,0 +1,17 @@
+//Painter Partition Problem
+//Pending
+
+#include<iostream>
+using namespace std;
+
+
+
+
+int main(){
+    int n = 5;
+
+    int arr[n] = {40,20,30,10,50};
+    cout<<allocateBooks(arr,n,3);
+
+
+ }
