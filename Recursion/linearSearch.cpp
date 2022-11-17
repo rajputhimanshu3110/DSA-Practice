877b6dd5b7aa07e4b466edb15ebef9186c4f58c5
Thakur Viraj Singh <75830024+rajputhimanshu3110@users.noreply.github.com>
2022-11-18 00:00:28 +0530

    Day 17

---
+++ Recursion/linearSearch.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+int linearSearch(int arr[],int key, int n){
+    if(n<0)
+        return -1;
+    if(n[arr]==key)
+        return n;
+    else
+        return linearSearch(arr,key,n-1);
+
+}
+
+
+int main(){
+    int n;
+    cout<<"ENter Size of array:";
+    cin>>n;
+
+    int* arr = new int[n];
+
+    cout<<"Enter values of array :"<<endl;
+
+    for(int i=0;i<n;i++){
+        cin>>i[arr];
+    }
+
+    int key;
+    cout<<"ENter key of array:";
+    cin>>key;
+
+    cout<<"Position : "<<linearSearch(arr,key,n);
+
+
+    return 0;
+
+
+
+
+}
